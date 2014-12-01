5944abc22f5bb9d40fde7d3527ae537906b95d57
Jeff Ward <jeff.ward@gmail.com>
2014-11-30 17:02:47 -0700

    tabs -> spaces / whitespace

--- src/server/Server.hx
+++ src/server/Server.hx
@@ -16,8 +16,8 @@ class Server {
   static public function main() {
     trace("hxScout");
 
-		var listener = Thread.create(FLMListener.start);
-		listener.sendMessage(Thread.current());
+    var listener = Thread.create(FLMListener.start);
+    listener.sendMessage(Thread.current());
 
     var s = new Socket();
     s.bind(new sys.net.Host("0.0.0.0"),7933); // hxScout client port
@@ -44,8 +44,8 @@ class Server {
             break;
           }
           else if (e=="Blocked") {
-						// no data from client
-					}
+            // no data from client
+          }
           else {
             // Unknown exception, rethrow
             throw e;
@@ -89,8 +89,8 @@ class FLMUtil
 {
   public static function send_policy_file(s:Socket)
   {
-		s.output.writeString('<cross-domain-policy><site-control permitted-cross-domain-policies="master-only"/><allow-access-from domain="*" to-ports="7934,7933"/></cross-domain-policy>');
-		s.output.writeByte(0);
+    s.output.writeString('<cross-domain-policy><site-control permitted-cross-domain-policies="master-only"/><allow-access-from domain="*" to-ports="7934,7933"/></cross-domain-policy>');
+    s.output.writeByte(0);
     s.close();
   }
 }
@@ -116,169 +116,169 @@ class FLMListener {
     var listener = Thread.create(FLMListener.start);
     listener.sendMessage(client_writer);
 
-		trace("Starting FLMReader["+inst_id+"]...");
- 
-		var frames:Array<Frame> = [];
-		var cur_frame = new Frame(0, inst_id);
-		var delta:Int = 0;
-		var first_enter = true;
-		var next_is_as = false;
-		var stack_strings:Array<String> = ["1-indexed"];
- 
-		var r = new Amf3Reader(flm_socket.input);
-		var connected = true;
-		while( connected ) {
- 
-			// Read next event blob.
-			var data:Map<String, Dynamic> = null;
-			try {
-				data = r.read();
-			} catch( e:Dynamic )	{
-				// Handle EOF gracefully
-				if (Type.getClass(e)==haxe.io.Eof) {
+    trace("Starting FLMReader["+inst_id+"]...");
+
+    var frames:Array<Frame> = [];
+    var cur_frame = new Frame(0, inst_id);
+    var delta:Int = 0;
+    var first_enter = true;
+    var next_is_as = false;
+    var stack_strings:Array<String> = ["1-indexed"];
+
+    var r = new Amf3Reader(flm_socket.input);
+    var connected = true;
+    while( connected ) {
+
+      // Read next event blob.
+      var data:Map<String, Dynamic> = null;
+      try {
+        data = r.read();
+      } catch( e:Dynamic )	{
+        // Handle EOF gracefully
+        if (Type.getClass(e)==haxe.io.Eof) {
           trace("FLMReader["+inst_id+"] closing...");
-					connected = false;
-					flm_socket.close();
+          connected = false;
+          flm_socket.close();
           trace("TODO: do we need to kill this thread somehow? Should be no external references...");
-					break;
-				}
-				// Handle flash requests for policy file
-				if ((e+"").indexOf("type-marker: 60")>0) {
-					trace("Got flash policy file request, writing response...");
-					flm_socket.input.readUntil(0);
-					FLMUtil.send_policy_file(cast(flm_socket));
-					break;
-				}
-				// Other errors, rethrow
+          break;
+        }
+        // Handle flash requests for policy file
+        if ((e+"").indexOf("type-marker: 60")>0) {
+          trace("Got flash policy file request, writing response...");
+          flm_socket.input.readUntil(0);
+          FLMUtil.send_policy_file(cast(flm_socket));
+          break;
+        }
+        // Other errors, rethrow
         trace("Uh oh, rethrowing: "+e);
-				throw e;
-			}
- 
-			if (data!=null) {
-				//trace(data);
-				var name:String = cast(data['name'], String);
- 
+        throw e;
+      }
+
+      if (data!=null) {
+        //trace(data);
+        var name:String = cast(data['name'], String);
+
         if (name=='.swf.name') {
           client_writer.sendMessage(haxe.Json.stringify({session_name:data['value'], inst_id:inst_id}));
         }
 
-				// - - - - - - - - - - - -
-				// Timing / Span / Delta
-				// - - - - - - - - - - - -
-				if (data['delta']!=null) {
-					// Delta without a span implies span = 0 ?
-					var t:Timing = { delta:data['delta'], span:data['span']==null?0:data['span'], prev:null, self_time:0 };
- 
-					cur_frame.duration.total += t.delta;
- 
-					if (t.span>cur_frame.duration.total) {
-						if (name!='.network.localconnection.idle') {
-							trace("Event larger ("+t.span+") than current frame ("+cur_frame.duration.total+"): "+data);
-						}
-					}
- 
-					var self_time = t.span;
-					var lookback = t.span-t.delta;
-					//trace("- Considering: "+data+", self="+self_time+", lookback="+lookback+" -- t="+t);
-					var tref:Timing = cur_frame.timing;
-					while (lookback>0 && tref!=null) {
-						if (lookback>=tref.delta) {
-							self_time -= tref.delta;
-						} else if (lookback>=tref.span) {
-							self_time -= tref.span;
-						} else {
-							//trace("	 -- does not include ("+tref.delta+"), dropping lookback to "+(lookback-tref.delta));
-						}
-						lookback -= tref.delta;
-						tref = tref.prev;
-					}
-					t.prev = cur_frame.timing;
-					t.self_time = self_time;
-					cur_frame.timing = t;
- 
-					//trace("	 -- Final self time: "+self_time);
-					if (self_time<0) throw "Umm, can't have negative self time!!";
- 
-					//trace("	 -- "+name+": "+self_time);
- 
-					//if (next_is_as) trace("next_is_as on: "+name);
- 
-					if (next_is_as || name.indexOf(".as.")==0) cur_frame.duration.as += self_time;
-					else if (name.indexOf(".gc.")==0) cur_frame.duration.gc += self_time;
-					else if (name.indexOf(".exit")==0) cur_frame.duration.other += self_time;
-					else if (name.indexOf(".rend.")==0) cur_frame.duration.rend += self_time;
-					else if (name.indexOf(".swf.")==0) cur_frame.duration.other += self_time;
-					else {
-						cur_frame.duration.other += self_time;
-						cur_frame.duration.unknown += self_time;
-						#if DEBUG_UNKNOWN
-							var debug:String = data['name']+":"+self_time;
-							cur_frame.unknown_names.push(debug);
-						#end
-						//if (cur_frame.unknown_names.indexOf(data['name'])<0) cur_frame.unknown_names.push(data['name']);
-					}
- 
-					// not sure should do it this way, maybe a list of event names instead?
-					next_is_as = name=='.as.event';
- 
-				} else {
-					// Span shouldn't occur without a delta
-					if (data['span']!=null) throw( "Span without a delta on: "+data);
-				}
- 
-				// - - - - - - - - - - - -
-				// Memory
-				// - - - - - - - - - - - -
-				if (name.indexOf(".mem.")==0 && data["value"]!=null) {
-					var type:String = name.substr(5);
-					//if (cur_frame.mem[type]==null) cur_frame.mem[type] = 0;
-					cur_frame.mem[type] = data["value"];
-				}
- 
-				// - - - - - - - - - - - -
-				// Sampler
-				// - - - - - - - - - - - -
-				if (name.indexOf(".sampler.")==0) {
-					if (name==".sampler.methodNameMap") {
+        // - - - - - - - - - - - -
+        // Timing / Span / Delta
+        // - - - - - - - - - - - -
+        if (data['delta']!=null) {
+          // Delta without a span implies span = 0 ?
+          var t:Timing = { delta:data['delta'], span:data['span']==null?0:data['span'], prev:null, self_time:0 };
+
+          cur_frame.duration.total += t.delta;
+
+          if (t.span>cur_frame.duration.total) {
+            if (name!='.network.localconnection.idle') {
+              trace("Event larger ("+t.span+") than current frame ("+cur_frame.duration.total+"): "+data);
+            }
+          }
+
+          var self_time = t.span;
+          var lookback = t.span-t.delta;
+          //trace("- Considering: "+data+", self="+self_time+", lookback="+lookback+" -- t="+t);
+          var tref:Timing = cur_frame.timing;
+          while (lookback>0 && tref!=null) {
+            if (lookback>=tref.delta) {
+              self_time -= tref.delta;
+            } else if (lookback>=tref.span) {
+              self_time -= tref.span;
+            } else {
+              //trace("	 -- does not include ("+tref.delta+"), dropping lookback to "+(lookback-tref.delta));
+            }
+            lookback -= tref.delta;
+            tref = tref.prev;
+          }
+          t.prev = cur_frame.timing;
+          t.self_time = self_time;
+          cur_frame.timing = t;
+
+          //trace("	 -- Final self time: "+self_time);
+          if (self_time<0) throw "Umm, can't have negative self time!!";
+
+          //trace("	 -- "+name+": "+self_time);
+
+          //if (next_is_as) trace("next_is_as on: "+name);
+
+          if (next_is_as || name.indexOf(".as.")==0) cur_frame.duration.as += self_time;
+          else if (name.indexOf(".gc.")==0) cur_frame.duration.gc += self_time;
+          else if (name.indexOf(".exit")==0) cur_frame.duration.other += self_time;
+          else if (name.indexOf(".rend.")==0) cur_frame.duration.rend += self_time;
+          else if (name.indexOf(".swf.")==0) cur_frame.duration.other += self_time;
+          else {
+            cur_frame.duration.other += self_time;
+            cur_frame.duration.unknown += self_time;
+#if DEBUG_UNKNOWN
+            var debug:String = data['name']+":"+self_time;
+            cur_frame.unknown_names.push(debug);
+#end
+            //if (cur_frame.unknown_names.indexOf(data['name'])<0) cur_frame.unknown_names.push(data['name']);
+          }
+
+          // not sure should do it this way, maybe a list of event names instead?
+          next_is_as = name=='.as.event';
+
+        } else {
+          // Span shouldn't occur without a delta
+          if (data['span']!=null) throw( "Span without a delta on: "+data);
+        }
+
+        // - - - - - - - - - - - -
+        // Memory
+        // - - - - - - - - - - - -
+        if (name.indexOf(".mem.")==0 && data["value"]!=null) {
+          var type:String = name.substr(5);
+          //if (cur_frame.mem[type]==null) cur_frame.mem[type] = 0;
+          cur_frame.mem[type] = data["value"];
+        }
+
+        // - - - - - - - - - - - -
+        // Sampler
+        // - - - - - - - - - - - -
+        if (name.indexOf(".sampler.")==0) {
+          if (name==".sampler.methodNameMap") {
             if (cur_frame.push_stack_strings==null) cur_frame.push_stack_strings = [];
-						var bytes = cast(data["value"], haxe.io.Bytes);
-						var start=0;
-						for (i in 0...bytes.length) {
-							var b = bytes.get(i);
-							if (b==0) {
+            var bytes = cast(data["value"], haxe.io.Bytes);
+            var start=0;
+            for (i in 0...bytes.length) {
+              var b = bytes.get(i);
+              if (b==0) {
                 var stack_string:String = bytes.getString(start, i-start);
                 cur_frame.push_stack_strings.push(stack_string);
-								stack_strings.push(stack_string);
-								start = i+1;
-							}
-						}
-						//trace("Stack strings now: "+stack_strings.length);
-						//for (i in 0...stack_strings.length) {
-						//  trace(i+": "+stack_strings[i]);
-						//}
-					}
-					else if (name==".sampler.sample") {
-						var value:Map<String,Dynamic> = data["value"];
+                stack_strings.push(stack_string);
+                start = i+1;
+              }
+            }
+            //trace("Stack strings now: "+stack_strings.length);
+            //for (i in 0...stack_strings.length) {
+            //  trace(i+": "+stack_strings[i]);
+            //}
+          }
+          else if (name==".sampler.sample") {
+            var value:Map<String,Dynamic> = data["value"];
             if (cur_frame.samples==null) cur_frame.samples = [];
-						cur_frame.samples.push(value);
+            cur_frame.samples.push(value);
             //trace(value);
-					}
-				}
- 
-				if (data['name']==".enter") {
-					if (first_enter) {
-						first_enter = false;
-					} else {
-						cur_frame.timing = null; // release timing events
-						//Sys.stdout().writeString(cur_frame.to_json()+",\n");
-						client_writer.sendMessage(cur_frame.to_json());
-						frames.push(cur_frame);
-						cur_frame = new Frame(cur_frame.id+1, inst_id);
-					}
-				}
-			}
-		}
-	}
+          }
+        }
+
+        if (data['name']==".enter") {
+          if (first_enter) {
+            first_enter = false;
+          } else {
+            cur_frame.timing = null; // release timing events
+            //Sys.stdout().writeString(cur_frame.to_json()+",\n");
+            client_writer.sendMessage(cur_frame.to_json());
+            frames.push(cur_frame);
+            cur_frame = new Frame(cur_frame.id+1, inst_id);
+          }
+        }
+      }
+    }
+  }
 }
 
 class Frame {
@@ -289,9 +289,9 @@ class Frame {
   public var samples:Array<Dynamic>;
   public var push_stack_strings:Array<String>;
   //public var events:Array<Dynamic>;
-  #if DEBUG_UNKNOWN
-    public var unknown_names:Array<String>;
-  #end
+#if DEBUG_UNKNOWN
+  public var unknown_names:Array<String>;
+#end
   public var timing:Timing;
 
   public function new(frame_id:Int, instance_id:Int) {
@@ -308,9 +308,9 @@ class Frame {
     mem = new Map<String, Int>();
     samples = null;
     push_stack_strings = null;
-    #if DEBUG_UNKNOWN
-      unknown_names = [];
-    #end
+#if DEBUG_UNKNOWN
+    unknown_names = [];
+#end
   }
 
   public function to_json():String
@@ -318,9 +318,9 @@ class Frame {
     return haxe.Json.stringify({
       id:id,
       inst_id:inst_id,
-      #if DEBUG_UNKNOWN
-          unknown_names:unknown_names,
-      #end
+#if DEBUG_UNKNOWN
+      unknown_names:unknown_names,
+#end
       duration:duration,
       push_stack_strings:push_stack_strings,
       samples:samples,

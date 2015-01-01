d97620376773ce224e9e0be78623ee7655e2c385
Jeff Ward <jeff.ward@cyclops.local>
2015-01-01 01:07:13 -0700

    Added windows packaging script (batch file)

---
+++ package_win.bat
@@ -0,0 +1,22 @@
+@echo off
+cd src\splash
+rmdir /s /q export
+
+echo Building splash project...
+call lime build windows > NUL
+cd ..\client
+
+rmdir /s /q export
+echo Building client project...
+call lime build windows > NUL
+
+cd ..
+set /p VER=<version.txt
+set TGT=hxScout-%VER: =_%
+echo Packaging %TGT%.zip
+copy splash\export\windows\cpp\bin\hxScoutSplash.exe client\export\windows\cpp\bin\ > NUL
+cd client\export\windows\cpp\
+ren bin %TGT%
+..\..\..\..\..\util\zip.exe -r %TGT%.zip %TGT% > NUL
+move %TGT%.zip ..\..\..\..\..\ > NUL
+cd ..\..\..\..\..\
